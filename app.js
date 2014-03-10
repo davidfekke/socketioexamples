fde6714baecb92efd26abfd3d4e1abaa60c86b44
David Fekke <david@fekke.com>
2014-03-10 09:29:52 -0400

    Set the port to 3000
    Set it back to process.env.port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var path = require('path');
 //var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000); //process.env.PORT || 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 //app.use(app.favicon());
