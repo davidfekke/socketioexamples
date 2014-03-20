9b9dfcddb6b5c4d90175b21bd01594f668d5cc5a
David Fekke <david@fekke.com>
2014-03-20 15:34:56 -0400

    Adding full HTML to samples

--- app.js
+++ app.js
@@ -12,24 +12,10 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var path = require('path');
 
-//var app = express();
-
 // all environments
 app.set('port', 3000); //process.env.PORT || 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-//app.use(app.favicon());
-//app.use(express.logger('dev'));
-//app.use(express.json());
-//app.use(express.urlencoded());
-//app.use(express.methodOverride());
-//app.use(app.router);
-//app.use(app.express.static(path.join(__dirname, 'public')));
-
-// development only
-//if ('development' == app.get('env')) {
-//  app.use(express.errorHandler());
-//}
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
